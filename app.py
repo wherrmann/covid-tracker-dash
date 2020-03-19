076420f28b170f37dc7a8799dc5a58cb2be4d0b2
Will Herrmann <herrmannwh@gmail.com>
2020-03-19 01:09:18 -0600

    not a web developer

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from dash.dependencies import Input, Output
 import requests
-import plotly.express as px
-import plotly.io as pio
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 import dash
 import dash_core_components as dcc
@@ -21,12 +21,14 @@ app.layout = html.Div(
             [
                 dcc.Dropdown(
                     id="state_dropdown",
+                    value="NY",
                     options=[{"label": label, "value": val} for val, label in state_mapping.items()],
                 )
             ],
-            className="app__dropdown",
+            className="app__dropdown"
         ),
         dcc.Graph(id="graph"),
+        html.Br(),
         html.Footer(
             [
                 html.Strong(dcc.Link('Created', href="https://github.com/wherrmann/covid-tracker-dash")),
@@ -51,18 +53,62 @@ def make_figure(value):
     df['date'] = pd.to_datetime(df['date'],format = '%Y%m%d')
     df = df.sort_values(by='date')
 
+    df['posNeg'] = df['positive'] + df['negative']
     df['new_positive'] = df['positive'].diff()
     df['new_total'] = df['total'].diff()
     df['positive_rate'] = df['positive']/df['posNeg']
-    df['daily_positive_rate'] = df['new_positive']/df['new_posNeg']
 
-    fig = px.bar(df,
-        x="date",
-        y="new_positive",
-        title ="Daily New Cases - {}".format(state_mapping[state])
+    fig = make_subplots(
+        rows=3,
+        cols=1,
+        subplot_titles=(
+            "Daily New Cases - {}".format(state_mapping[state]),
+            "Daily New Tests Administered - {}".format(state_mapping[state]),
+            "Positive Test Rate, Aggregate - {}".format(state_mapping[state]),
+        ),
+        x_title="Date",
+        vertical_spacing=0.1
     )
-    fig.update_xaxes(title_text='Date')
-    fig.update_yaxes(title_text='Confirmed Cases')
+
+    fig.add_trace(
+        go.Bar(
+            x=df['date'],
+            y=df['new_positive'],
+            name=""
+        ),
+        row=1,
+        col=1
+    )
+    fig.add_trace(
+        go.Bar(
+            x=df['date'],
+            y=df['new_total'],
+            name=""
+        ),
+        row=2,
+        col=1
+    )
+    fig.add_trace(
+        go.Bar(
+            x=df['date'],
+            y=df['positive_rate'],
+            name=""
+        ),
+        row=3,
+        col=1
+    )
+    fig.update_yaxes(title_text="Confirmed Cases", row=1, col=1)
+    fig.update_yaxes(title_text="Tests Administered", row=2, col=1)
+    fig.update_yaxes(title_text="Positive Test Rate", tickformat = ',.0%', row=3, col=1)
+    fig.update_layout(
+        font=dict(
+            family="Raleway, monospace",
+            size=18,
+            color="#222"
+        ),
+        showlegend=False,
+        height = 1400
+    )
     return fig
 
 if __name__ == '__main__':
