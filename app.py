23bd1862677eb42ddbebcf1d50a2394b7539862c
Will Herrmann <herrmannwh@gmail.com>
2020-03-19 21:11:29 -0600

    clean up and delete a lot of code

--- app.py
+++ app.py
@@ -11,6 +11,13 @@ import dash_html_components as html
 
 from metadata.states import STATE_MAPPING, STATE_POP
 
+FIG_FONT_DICT = {
+    'family': "Raleway, monospace",
+    'size' : 18,
+    'color' : "#222"
+}
+BASE_API_URL = 'https://covidtracking.com/api/'
+
 app = dash.Dash(__name__)
 
 app.config.suppress_callback_exceptions = True
@@ -44,14 +51,31 @@ app.layout = html.Div(
     ]
 )
 
-def make_us_agg_figures():
-    url = 'https://covidtracking.com/api/us/daily'
+def get_data(endpoint):
+    """
+    Retrieve foundational data from covidtracking.com.
+    """
+    base_url = BASE_API_URL
+    url = '/'.join([base_url,endpoint])
     r = requests.get(url)
     data = r.json()
+    return data
 
+def make_bar_figures(endpoint, region):
+    """
+    Makes three figure plotly subplot with these metrics:
+     - Daily new cases
+     - Daily new tests administered
+     - Aggregate positive test rate
+    """
+    data = get_data(endpoint)
     df = pd.DataFrame(data)
     df['date'] = pd.to_datetime(df['date'],format = '%Y%m%d')
     df = df.sort_values(by='date')
+
+    # posNeg isn't returned by every endpoint
+    if 'posNeg' not in list(df.columns):
+        df['posNeg'] = df['positive'] + df['negative']
 
     df['new_positive'] = df['positive'].diff()
     df['new_total'] = df['total'].diff()
@@ -61,14 +85,13 @@ def make_us_agg_figures():
         rows=3,
         cols=1,
         subplot_titles=(
-            "Daily New Cases - {}".format('US'),
-            "Daily New Tests Administered - {}".format('US'),
-            "Positive Test Rate, Aggregate - {}".format('US')
+            "Daily New Cases - {}".format(region),
+            "Daily New Tests Administered - {}".format(region),
+            "Positive Test Rate, Aggregate - {}".format(region)
         ),
         x_title="Date",
         vertical_spacing=0.1
     )
-
     fig.add_trace(
         go.Bar(
             x=df['date'],
@@ -100,25 +123,29 @@ def make_us_agg_figures():
     fig.update_yaxes(title_text="Tests Administered", row=2, col=1)
     fig.update_yaxes(title_text="Positive Test Rate", tickformat = ',.0%', row=3, col=1)
     fig.update_layout(
-        font=dict(
-            family="Raleway, monospace",
-            size=18,
-            color="#222"
-        ),
+        font=FIG_FONT_DICT,
         showlegend=False,
         height = 1400
     )
     return fig
 
-
 def make_map_figures():
-    r = requests.get('https://covidtracking.com/api/states')
-    data = r.json()
+    """
+    Makes three choropleth mapbox figures of the US for these metrics:
+     - Cases per capita
+     - Tests per capita
+     - Positive test rate
+    """
+    data = get_data('states')
     for state in data:
         state['state_name'] = STATE_MAPPING[state['state']]
     raw_df = pd.DataFrame(data)
+
+    # get geojson data for use in mapping states
     geojson = requests.get('https://eric.clst.org/assets/wiki/uploads/Stuff/gz_2010_us_040_00_500k.json')
     states_geo = geojson.json()
+
+    # get state population data from census.gov
     states_pop = STATE_POP
     states_pop_df = pd.DataFrame(states_pop['data'])
     df = pd.merge(raw_df,states_pop_df,left_on='state_name',right_on='State')
@@ -131,71 +158,43 @@ def make_map_figures():
     df['positives_per_million'] = df['positives_per_capita']*1000000
     df['tests_per_million'] = df['tests_per_capita']*1000000
 
+    layout_dict = {'font': FIG_FONT_DICT}
+
+    standard_choropleth_mapbox_args = {
+        'locations' : "state_name",
+        'geojson' : states_geo,
+        'featureidkey' : "properties.NAME",
+        'color_continuous_scale' : "viridis_r",
+        'opacity' : 0.5,
+        'center' : {"lat": 37.0902, "lon": -95.7129},
+        'zoom' : 3,
+        'mapbox_style' : "carto-positron",
+        'height' : 800
+    }
+
     fig1 = px.choropleth_mapbox(df[df['positives_per_million'].notna()],
-        locations="state_name",
-        geojson=states_geo,
-        featureidkey="properties.NAME",
-        color="positives_per_million",
-        color_continuous_scale="viridis_r",
-        opacity=0.5,
         title ="Confirmed Cases per Million People",
-        center = {"lat": 37.0902, "lon": -95.7129},
-        zoom=3,
-        mapbox_style="carto-positron",
-        height = 800
+        color="positives_per_million",
+        **standard_choropleth_mapbox_args
     )
     fig1.update(
-        layout=dict(
-            font=dict(
-                family="Raleway, monospace",
-                size=18,
-                color="#222"
-            )
-        )
+        layout=layout_dict
     )
     fig2 = px.choropleth_mapbox(df[df['tests_per_million'].notna()],
-        locations="state_name",
-        geojson=states_geo,
-        featureidkey="properties.NAME",
-        color="tests_per_million",
-        color_continuous_scale="viridis_r",
-        opacity=0.5,
         title ="Tests Per Million People",
-        center = {"lat": 37.0902, "lon": -95.7129},
-        zoom=3,
-        mapbox_style="carto-positron",
-        height = 800
+        color="tests_per_million",
+        **standard_choropleth_mapbox_args
     )
     fig2.update(
-        layout=dict(
-            font=dict(
-                family="Raleway, monospace",
-                size=18,
-                color="#222"
-            )
-        )
+        layout=layout_dict
     )
     fig3 = px.choropleth_mapbox(df[df['positives_per_hundred_tests'].notna()],
-        locations="state_name",
-        geojson=states_geo,
-        featureidkey="properties.NAME",
-        color="positives_per_hundred_tests",
-        color_continuous_scale="viridis_r",
-        opacity=0.5,
         title ="Positives per Hundred Tests Administered",
-        center = {"lat": 37.0902, "lon": -95.7129},
-        zoom=3,
-        mapbox_style="carto-positron",
-        height = 800
+        color="positives_per_hundred_tests",
+        **standard_choropleth_mapbox_args
     )
     fig3.update(
-        layout=dict(
-            font=dict(
-                family="Raleway, monospace",
-                size=18,
-                color="#222"
-            )
-        )
+        layout=layout_dict
     )
     graphs_div = html.Div([
         dcc.Graph(id='graph-map-1',figure=fig1),
@@ -204,13 +203,12 @@ def make_map_figures():
     ])
     return graphs_div
 
-
 @app.callback(Output('tabs-content', 'children'),
               [Input('tabs-covid', 'value')])
 def render_content(tab):
     if tab == 'us':
         return html.Div([
-            dcc.Graph(id='graph-us',figure=make_us_agg_figures())
+            dcc.Graph(id='graph-us',figure=make_bar_figures('us/daily','US'))
         ])
     elif tab == 'states':
         content = html.Div([
@@ -232,72 +230,7 @@ def render_content(tab):
 
 @app.callback(Output("state-graphs", "figure"), [Input("state_dropdown", "value")])
 def make_figure(value):
-    state = value
-    url = 'https://covidtracking.com/api/states/daily?state={}'.format(state)
-    r = requests.get(url)
-    data = r.json()
-
-    df = pd.DataFrame(data)
-    df['date'] = pd.to_datetime(df['date'],format = '%Y%m%d')
-    df = df.sort_values(by='date')
-
-    df['posNeg'] = df['positive'] + df['negative']
-    df['new_positive'] = df['positive'].diff()
-    df['new_total'] = df['total'].diff()
-    df['positive_rate'] = df['positive']/df['posNeg']
-
-    fig = make_subplots(
-        rows=3,
-        cols=1,
-        subplot_titles=(
-            "Daily New Cases - {}".format(STATE_MAPPING[state]),
-            "Daily New Tests Administered - {}".format(STATE_MAPPING[state]),
-            "Positive Test Rate, Aggregate - {}".format(STATE_MAPPING[state]),
-        ),
-        x_title="Date",
-        vertical_spacing=0.1
-    )
-
-    fig.add_trace(
-        go.Bar(
-            x=df['date'],
-            y=df['new_positive'],
-            name=""
-        ),
-        row=1,
-        col=1
-    )
-    fig.add_trace(
-        go.Bar(
-            x=df['date'],
-            y=df['new_total'],
-            name=""
-        ),
-        row=2,
-        col=1
-    )
-    fig.add_trace(
-        go.Bar(
-            x=df['date'],
-            y=df['positive_rate'],
-            name=""
-        ),
-        row=3,
-        col=1
-    )
-    fig.update_yaxes(title_text="Confirmed Cases", row=1, col=1)
-    fig.update_yaxes(title_text="Tests Administered", row=2, col=1)
-    fig.update_yaxes(title_text="Positive Test Rate", tickformat = ',.0%', row=3, col=1)
-    fig.update_layout(
-        font=dict(
-            family="Raleway, monospace",
-            size=18,
-            color="#222"
-        ),
-        showlegend=False,
-        height = 1400
-    )
-    return fig
+    return make_bar_figures('states/daily?state={}'.format(value),STATE_MAPPING[value])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
